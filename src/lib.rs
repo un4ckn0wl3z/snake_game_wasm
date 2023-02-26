f32b71d2d4ee61bc4be6d6ded2c7a2a7c6898c9d
Anuwat <admin@unknownclub.net>
2023-02-26 21:32:52 +0700

    feat: use wee_alloc for optimize .wasm file size

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
 use wasm_bindgen::prelude::*;
 
+extern crate wee_alloc;
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+
 #[wasm_bindgen]
 pub fn greeting(name: &str) {
     alert(name);

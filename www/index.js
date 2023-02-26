9bc8e20c85953dc061b87e36b027fa8d9030e695
Anuwat <admin@unknownclub.net>
2023-02-26 19:50:45 +0700

    feat: binding wasm with js

--- www/index.js
+++ www/index.js
@@ -1,26 +1,5 @@
+import init, { greeting } from 'snake_game_wasm';
 
-
-
-async function init() {
-
-    const importObject = {
-      console: {
-        log: () => {
-          console.log("Just logging something!");
-        },
-        error: () => {
-          console.log("I am just error");
-        }
-      }
-    }
-  
-    const response = await fetch("sum.wasm");
-    const buffer = await response.arrayBuffer();
-    const wasm = await WebAssembly.instantiate(buffer, importObject);
-  
-    const sumFunction = wasm.instance.exports.sum;
-    const result = sumFunction(100, 300);
-    console.log(result);
-  }
-  
-  init();
+init().then(_ => {
+  greeting("Anuwat");
+});

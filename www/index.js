e384cb6ea43968e2debb72bd725cd27e10bbb241
Anuwat <admin@unknownclub.net>
2023-02-26 19:16:39 +0700

    feat: update webpack config

--- www/index.js
+++ www/index.js
@@ -1,11 +1,26 @@
+
+
+
 async function init() {
 
-    const response = await fetch('sum.wasm');
+    const importObject = {
+      console: {
+        log: () => {
+          console.log("Just logging something!");
+        },
+        error: () => {
+          console.log("I am just error");
+        }
+      }
+    }
+  
+    const response = await fetch("sum.wasm");
     const buffer = await response.arrayBuffer();
-
-    const wasm = await WebAssembly.instantiate(buffer);
+    const wasm = await WebAssembly.instantiate(buffer, importObject);
+  
     const sumFunction = wasm.instance.exports.sum;
-    const result = sumFunction(500, 1000);
+    const result = sumFunction(100, 300);
     console.log(result);
-}
-init();
+  }
+  
+  init();

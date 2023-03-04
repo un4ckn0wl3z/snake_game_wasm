0105dad2efa67642e3588cf81464d83b63c9b936
Anuwat <admin@unknownclub.net>
2023-03-04 21:42:37 +0700

    feat: change snake color

--- www/index.ts
+++ www/index.ts
@@ -1,5 +1,6 @@
-import init, { World, Direction } from 'snake_game_wasm';
- 
+
+import init, { World, Direction } from "snake_game_wasm";
+
 init().then(wasm => {
   const CELL_SIZE = 20;
   const WORLD_WIDTH = 8;
@@ -10,53 +11,44 @@ init().then(wasm => {
 
   const canvas = <HTMLCanvasElement> document.getElementById("snake-canvas");
   const ctx = canvas.getContext("2d");
+
   canvas.height = worldWidth * CELL_SIZE;
   canvas.width = worldWidth * CELL_SIZE;
-   
-  const snakeCellPtr = world.snake_cells();
-  const snakeLen = world.snake_length();
-  const snakeCells = new Uint32Array(
-    wasm.memory.buffer, snakeCellPtr, snakeLen
-  );
- 
 
-  document.addEventListener("keydown", (e) => {
-    switch (e.code) {
+  document.addEventListener("keydown", e => {
+    switch(e.code) {
       case "ArrowUp":
-        world.change_snake_dir(Direction.Up)
+        world.change_snake_dir(Direction.Up);
         break;
       case "ArrowRight":
-        world.change_snake_dir(Direction.Right)
+        world.change_snake_dir(Direction.Right);
         break;
       case "ArrowDown":
-        world.change_snake_dir(Direction.Down)
+        world.change_snake_dir(Direction.Down);
         break;
       case "ArrowLeft":
-        world.change_snake_dir(Direction.Left)
-        break;
-      default:
+        world.change_snake_dir(Direction.Left);
         break;
     }
-  });
+  })
 
-  function drawWorld(){
+  function drawWorld() {
     ctx.beginPath();
 
     for (let x = 0; x < worldWidth + 1; x++) {
       ctx.moveTo(CELL_SIZE * x, 0);
-      ctx.lineTo(CELL_SIZE * x, worldWidth * CELL_SIZE);
+      ctx.lineTo(CELL_SIZE * x, worldWidth * CELL_SIZE)
     }
 
     for (let y = 0; y < worldWidth + 1; y++) {
       ctx.moveTo(0, CELL_SIZE * y);
-      ctx.lineTo(worldWidth * CELL_SIZE, CELL_SIZE * y);
+      ctx.lineTo(worldWidth * CELL_SIZE, CELL_SIZE * y)
     }
 
     ctx.stroke();
   }
 
-  function drawSnake(){
-
+  function drawSnake() {
     const snakeCells = new Uint32Array(
       wasm.memory.buffer,
       world.snake_cells(),
@@ -68,9 +60,8 @@ init().then(wasm => {
       const row = Math.floor(cellIdx / worldWidth);
 
       ctx.fillStyle = i === 0 ? "#7878db" : "#000000";
-      
+
       ctx.beginPath();
-  
       ctx.fillRect(
         col * CELL_SIZE,
         row * CELL_SIZE,
@@ -79,30 +70,25 @@ init().then(wasm => {
       );
     })
 
-
-
-
     ctx.stroke();
   }
 
-  function paint(){
+  function paint() {
     drawWorld();
     drawSnake();
   }
 
-  function update(){
-    const fps = 7;
+  function update() {
+    const fps = 10;
     setTimeout(() => {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       world.update();
       paint();
-      // the method take a callback to invoked before the next repaint
-      requestAnimationFrame(update);
-    }, 1000 / fps);
+      // the method takes a callback to invoked before the next repaint
+      requestAnimationFrame(update)
+    }, 1000 / fps)
   }
 
   paint();
   update();
- 
-
-});
+})

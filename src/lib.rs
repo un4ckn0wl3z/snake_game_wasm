f3157ba6a4d2cebb8fe6e496bc6fc90eb3139342
Anuwat <admin@unknownclub.net>
2023-03-13 21:16:24 +0700

    feat: moving snake cell

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,7 @@ extern {
     pub fn alert(s: &str);
 }
 
+#[derive(Clone)]
 pub struct SnakeCell(usize);
 
 struct Snake {
@@ -87,8 +88,15 @@ impl World {
 
 
     pub fn step(&mut self) {
+        let temp = self.snake.body.clone();
         let next_cell = self.gen_next_snake_cell();
         self.snake.body[0] = next_cell;
+
+        let len = self.snake.body.len();
+        for i in 1..len {
+            self.snake.body[i] = SnakeCell(temp[i - 1].0)
+        }
+
     }
 
     fn gen_next_snake_cell(&self) -> SnakeCell {
